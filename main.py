79b7d7d957e662d7cb6473f20729d544967eeb7d
mojtabagolab <mojtabagolab10@gmail.com>
2024-04-06 14:45:32 +0330

    part11

--- main.py
+++ main.py
@@ -138,6 +138,9 @@ def Login():
             #check user passwrod for login 
             if valueEditText == App_Password_Decrypt :
                 messagebox.showinfo("Success" , "Password is correct") ##alert dialog successfully login
+                #close Window Login and open window Menu
+                Window_Login.destroy()
+                Menu()
             else:
                 messagebox.showerror("Error" , "Password is wrong ") #alert dialog error login
 
@@ -154,6 +157,88 @@ def Login():
     
 
 #________________________________End login gui code_________________________________
+#________________________________Menu gui code_________________________________
+def Menu():
+    #---------------------------------------------------
+    #variable class Tk from tkinter lib
+    Window_Menu = Tk()
+    #---------------------------------------------------
+    #customize window app
+    Window_Menu.title("PASSAM (menu)")
+    Window_Menu.config(bg= "#353535")
+    Window_Menu.geometry("800x500")
+    Window_Menu.resizable(False , False)
+    #enter your image.ico location with two \\
+    Window_Menu.iconbitmap("E:\\project\\PasswordManager\\iconApp.ico")
+    #---------------------------------------------------
+    #text Menu in header app
+    Text_Menu = Label(Window_Menu , text="Passam" , bg="#353535" , fg="white" , width=10)
+    Text_Menu.pack()
+    #Text_Welcome.place(x=250 , y=20)
+    Text_Menu_Style = ("Caveat" , 50 , "bold")
+    Text_Menu.configure(font=Text_Menu_Style)
+    
+    #---------------------------------------------------
+    #the frame box item button add 
+    Frame_Add = Frame(Window_Menu , bg="#555454" , width=150 , height=150)
+    Frame_Add.pack()
+    Frame_Add.place(x=150 , y=120)
+    #image button add
+    Add_Image = Image.open("add.png")
+    Add_Image = Add_Image.resize((100 , 100))
+    Add_Image = ImageTk.PhotoImage(Add_Image)
+    #define a function to be called when the button is clicked
+    def button_add_clicked():
+        pass
+    #create the button with the image
+    Button_add = Button(Frame_Add ,image= Add_Image , command=button_add_clicked)
+    Button_add.pack()
+    Button_add.image_names = Add_Image
+    Button_add.place(x=23 , y=23)
+    
+    #---------------------------------------------------
+    #the frame box item button delete 
+    Frame_Del = Frame(Window_Menu , bg="#555454" , width=150 , height=150)
+    Frame_Del.pack()
+    Frame_Del.place(x=500 , y=120)
+    #image button delete
+    Del_Image = Image.open("delete.png")
+    Del_Image = Del_Image.resize((100 , 100))
+    Del_Image = ImageTk.PhotoImage(Del_Image)
+    #define a function to be called when the button is clicked
+    def button_del_clicked():
+        pass
+    #create the button with the image
+    Button_Del = Button(Frame_Del ,image= Del_Image , command=button_del_clicked)
+    Button_Del.pack()
+    Button_Del.image_names = Del_Image
+    Button_Del.place(x=23 , y=23)
+    
+    #---------------------------------------------------
+     #the frame box item button view 
+    Frame_View = Frame(Window_Menu , bg="#555454" , width=150 , height=150)
+    Frame_View.pack()
+    Frame_View.place(x=325 , y=300)
+    #image button View
+    View_Image = Image.open("list.png")
+    View_Image = View_Image.resize((100,100))
+    View_Image = ImageTk.PhotoImage(View_Image)
+    #define a function to be called when the button is clicked
+    def button_view_clicked():
+        pass
+    #create the button with the image
+    Button_View = Button(Frame_View ,image= View_Image , command=button_view_clicked)
+    Button_View.pack()
+    Button_View.image_names = View_Image
+    Button_View.place(x=23 , y=23)
+    #---------------------------------------------------
+    
+    #---------------------------------------------------
+    #start window 
+    Window_Menu.mainloop
+    #---------------------------------------------------
+#________________________________End Menu gui code_________________________________
+#_________________________________End GUI CODE_____________________________
 
 #_________________________________________________________
 #creat text file for check user statment
